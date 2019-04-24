6468fd46bf6dce86487844e195b238a9dec5ed79
Adesoji Daniel <sojidaniels@gmail.com>
2019-04-24 22:25:04 +0100

    feature(parties):implement get all parties feature [finishes #165563945, #165563955]

--- src/components/common/PartyPage.jsx
+++ src/components/common/PartyPage.jsx
@@ -1,10 +1,65 @@
 import React, { Component } from 'react';
+import partyService from '../../services/parties';
 
 class PartyPage extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      parties: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    const allParties = await partyService.getAllParties();
+    this.setState({ parties: allParties.data });
+  };
+
+  partylogo = logoUrl => {
+    const localUrl = 'http://127.0.0.1:3000/api/v1';
+    const url = `${localUrl}`;
+    if (logoUrl === 'logo123') {
+      return 'No logo';
+    }
+
+    return `<img src="${url}/images/${logoUrl}"></img>`;
+  };
 
   render() {
-    return <div>This is the party page</div>;
+    const { parties } = this.state;
+    const listOfParties = parties.map(party => (
+      <tr key={party.id}>
+        <td>{party.name}</td>
+        <td>{party.hqaddress}</td>
+        <td>{this.partylogo(party.logourl)}</td>
+      </tr>
+    ));
+
+    const noParties = (
+      <div className="center">
+        <h2>No Parties</h2>
+      </div>
+    );
+
+    return (
+      <div>
+        <div>
+          {listOfParties.length ? (
+            <table>
+              <tbody>
+                <tr>
+                  <th>Party Name</th>
+                  <th>HQ</th>
+                  <th>Party Logo</th>
+                </tr>
+              </tbody>
+              <tbody>{listOfParties}</tbody>
+            </table>
+          ) : (
+            <div>{noParties}</div>
+          )}
+        </div>
+      </div>
+    );
   }
 }
 
