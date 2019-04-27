e2b94612972a8a56e885edc4ce3de6a6499ee67f
Adesoji Daniel <sojidaniels@gmail.com>
2019-04-27 21:05:59 +0100

    feature(redux): update test cases

--- __tests__/components/LoginPage.test.jsx
+++ __tests__/components/LoginPage.test.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
-// Components
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
 import LoginPage from '../../src/components/LoginPage';
+
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 describe('<LoginPage />', () => {
   it('renders login page', () => {
-    const wrapper = shallow(<LoginPage />);
+    const wrapper = shallow(
+      <Provider store={store}>
+        <LoginPage />
+      </Provider>
+    );
     expect(wrapper.find('div'));
   });
 });
--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,46 +1,18 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
-import jwt from 'jsonwebtoken';
 import Navigation from './common/Sidenav';
 import Header from './common/Header';
 
 class UserPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      // expired: false,
-      // noUser: false,
-    };
+    this.state = {};
   }
 
-  // checkExpire = () => {
-  //   const data = jwt.decode(localStorage.token);
-  //   const currentTime = new Date().getTime() / 1000;
-  //   if (currentTime > data.exp) {
-  //     this.setState({ expired: true });
-  //   }
-  //   localStorage.clear();
-  // };
-
-  // checkToken = () => {
-  //   if (!localStorage.token) {
-  //     this.setState({ noUser: true });
-  //   }
-  // };
-
-  // componentDidMount = () => {
-  //   this.checkToken();
-  //   this.checkExpire();
-  // }
-
   render() {
-    // const { expired, noUser } = this.state;
     return (
       <div>
         <Header />
         <Navigation />
-        {/* {noUser && <Redirect to="login" />}
-        {expired && <Redirect to="login" />} */}
       </div>
     );
   }
